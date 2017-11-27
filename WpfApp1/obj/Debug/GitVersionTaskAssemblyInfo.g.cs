fe26ff0ac572737c6b422fb243420e860cb3d9b4
gneil90 <nail.galiaskarov@recordsure.com>
2017-11-27 10:08:20 +0600

    Semver remove

--- WpfApp1/obj/Debug/GitVersionTaskAssemblyInfo.g.cs
+++ WpfApp1/obj/Debug/GitVersionTaskAssemblyInfo.g.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 
 [assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyFileVersion("0.1.0.0")]
-[assembly: AssemblyInformationalVersion("0.1.0+2.Branch.master.Sha.7ce7b27bfb32e49b7efffa7681693d3d5a3a01aa")]
+[assembly: AssemblyInformationalVersion("0.1.0+3.Branch.master.Sha.ede287e457652f9b51528acad4201f3fec15b4bd")]
 
 namespace WpfApp1
 {
@@ -28,22 +28,22 @@ namespace WpfApp1
         public static string PreReleaseTagWithDash = "";
         public static string PreReleaseLabel = "";
         public static string PreReleaseNumber = "";
-        public static string BuildMetaData = "2";
-        public static string BuildMetaDataPadded = "0002";
-        public static string FullBuildMetaData = "2.Branch.master.Sha.7ce7b27bfb32e49b7efffa7681693d3d5a3a01aa";
+        public static string BuildMetaData = "3";
+        public static string BuildMetaDataPadded = "0003";
+        public static string FullBuildMetaData = "3.Branch.master.Sha.ede287e457652f9b51528acad4201f3fec15b4bd";
         public static string MajorMinorPatch = "0.1.0";
         public static string SemVer = "0.1.0";
         public static string LegacySemVer = "0.1.0";
         public static string LegacySemVerPadded = "0.1.0";
         public static string AssemblySemVer = "0.1.0.0";
-        public static string FullSemVer = "0.1.0+2";
-        public static string InformationalVersion = "0.1.0+2.Branch.master.Sha.7ce7b27bfb32e49b7efffa7681693d3d5a3a01aa";
+        public static string FullSemVer = "0.1.0+3";
+        public static string InformationalVersion = "0.1.0+3.Branch.master.Sha.ede287e457652f9b51528acad4201f3fec15b4bd";
         public static string BranchName = "master";
-        public static string Sha = "7ce7b27bfb32e49b7efffa7681693d3d5a3a01aa";
+        public static string Sha = "ede287e457652f9b51528acad4201f3fec15b4bd";
         public static string NuGetVersionV2 = "0.1.0";
         public static string NuGetVersion = "0.1.0";
-        public static string CommitsSinceVersionSource = "2";
-        public static string CommitsSinceVersionSourcePadded = "0002";
+        public static string CommitsSinceVersionSource = "3";
+        public static string CommitsSinceVersionSourcePadded = "0003";
         public static string CommitDate = "2017-11-26";
     }
 
